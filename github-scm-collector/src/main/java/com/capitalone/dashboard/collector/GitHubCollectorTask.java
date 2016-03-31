5bc56268514b37ff6f3507d10f9bffe216e83c60
Julien Audibert <julien.audibert@orange.com>
2016-03-31 14:18:56 +0200

    Suppression de warnings

--- github-scm-collector/src/main/java/com/capitalone/dashboard/collector/GitHubCollectorTask.java
+++ github-scm-collector/src/main/java/com/capitalone/dashboard/collector/GitHubCollectorTask.java
@@ -28,51 +28,51 @@ import java.util.Set;
  */
 @Component
 public class GitHubCollectorTask extends CollectorTask<Collector> {
-    private static final Log LOG = LogFactory.getLog(GitHubCollectorTask.class);
+	private static final Log LOG = LogFactory.getLog(GitHubCollectorTask.class);
 
-    private final BaseCollectorRepository<Collector> collectorRepository;
-    private final GitHubRepoRepository gitHubRepoRepository;
-    private final CommitRepository commitRepository;
-    private final GitHubClient gitHubClient;
-    private final GitHubSettings gitHubSettings;
-    private final ComponentRepository dbComponentRepository;
+	private final BaseCollectorRepository<Collector> collectorRepository;
+	private final GitHubRepoRepository gitHubRepoRepository;
+	private final CommitRepository commitRepository;
+	private final GitHubClient gitHubClient;
+	private final GitHubSettings gitHubSettings;
+	private final ComponentRepository dbComponentRepository;
 
-    @Autowired
-    public GitHubCollectorTask(TaskScheduler taskScheduler,
-                                   BaseCollectorRepository<Collector> collectorRepository,
-                                   GitHubRepoRepository gitHubRepoRepository,
-                                   CommitRepository commitRepository,
-                                   GitHubClient gitHubClient,
-                                   GitHubSettings gitHubSettings,
-                                   ComponentRepository dbComponentRepository) {
-        super(taskScheduler, "GitHub");
-        this.collectorRepository = collectorRepository;
-        this.gitHubRepoRepository = gitHubRepoRepository;
-        this.commitRepository = commitRepository;
-        this.gitHubClient = gitHubClient;
-        this.gitHubSettings = gitHubSettings;
-        this.dbComponentRepository = dbComponentRepository;
-    }
+	@Autowired
+	public GitHubCollectorTask(TaskScheduler taskScheduler,
+			BaseCollectorRepository<Collector> collectorRepository,
+			GitHubRepoRepository gitHubRepoRepository,
+			CommitRepository commitRepository,
+			GitHubClient gitHubClient,
+			GitHubSettings gitHubSettings,
+			ComponentRepository dbComponentRepository) {
+		super(taskScheduler, "GitHub");
+		this.collectorRepository = collectorRepository;
+		this.gitHubRepoRepository = gitHubRepoRepository;
+		this.commitRepository = commitRepository;
+		this.gitHubClient = gitHubClient;
+		this.gitHubSettings = gitHubSettings;
+		this.dbComponentRepository = dbComponentRepository;
+	}
 
-    @Override
-    public Collector getCollector() {
-        Collector protoType = new Collector();
-        protoType.setName("GitHub");
-        protoType.setCollectorType(CollectorType.SCM);
-        protoType.setOnline(true);
-        protoType.setEnabled(true);
-        return protoType;
-    }
+	@Override
+	public Collector getCollector() {
+		Collector protoType = new Collector();
+		protoType.setName("GitHub");
+		protoType.setCollectorType(CollectorType.SCM);
+		protoType.setOnline(true);
+		protoType.setEnabled(true);
+		return protoType;
+	}
 
-    @Override
-    public BaseCollectorRepository<Collector> getCollectorRepository() {
-        return collectorRepository;
-    }
+	@Override
+	public BaseCollectorRepository<Collector> getCollectorRepository() {
+		return collectorRepository;
+	}
 
-    @Override
-    public String getCron() {
-        return gitHubSettings.getCron();
-    }
+	@Override
+	public String getCron() {
+		return gitHubSettings.getCron();
+	}
 
 	/**
 	 * Clean up unused deployment collector items
@@ -80,7 +80,7 @@ public class GitHubCollectorTask extends CollectorTask<Collector> {
 	 * @param collector
 	 *            the {@link UDeployCollector}
 	 */
-    @SuppressWarnings("PMD.AvoidDeeplyNestedIfStmts") // agreed, fixme
+
 	private void clean(Collector collector) {
 		Set<ObjectId> uniqueIDs = new HashSet<ObjectId>();
 		/**
@@ -88,14 +88,13 @@ public class GitHubCollectorTask extends CollectorTask<Collector> {
 		 * Store their IDs in a unique set ONLY if their collector IDs match with GitHub collectors ID.
 		 */
 		for (com.capitalone.dashboard.model.Component comp : dbComponentRepository.findAll()) {
-			if (comp.getCollectorItems() != null && !comp.getCollectorItems().isEmpty()) {
+			if (comp.getCollectorItems() != null && !comp.getCollectorItems().isEmpty() && comp.getCollectorItems().get(CollectorType.SCM) != null) {
 				List<CollectorItem> itemList = comp.getCollectorItems().get(CollectorType.SCM);
-				if (itemList != null) {
-					for (CollectorItem ci : itemList) {
-						if (ci != null && ci.getCollectorId().equals(collector.getId())){
-							uniqueIDs.add(ci.getId());
-						}
+				for (CollectorItem ci : itemList) {
+					if (ci != null && ci.getCollectorId().equals(collector.getId())){
+						uniqueIDs.add(ci.getId());
 					}
+
 				}
 			}
 		}
@@ -117,51 +116,51 @@ public class GitHubCollectorTask extends CollectorTask<Collector> {
 	}
 
 
-    @Override
-    public void collect(Collector collector) {
+	@Override
+	public void collect(Collector collector) {
 
-        logBanner("Starting...");
-        long start = System.currentTimeMillis();
-        int repoCount = 0;
-        int commitCount = 0;
+		logBanner("Starting...");
+		long start = System.currentTimeMillis();
+		int repoCount = 0;
+		int commitCount = 0;
 
-        clean(collector);
-        for (GitHubRepo repo : enabledRepos(collector)) {
-        	boolean firstRun = false;
-        	if (repo.getLastUpdateTime() == null) firstRun = true;
-        	repo.setLastUpdateTime(new Date());
-            gitHubRepoRepository.save(repo);
-            LOG.debug(repo.getOptions().toString()+"::"+repo.getBranch());
-            for (Commit commit : gitHubClient.getCommits(repo, firstRun)) {
-            	LOG.debug(commit.getTimestamp()+":::"+commit.getScmCommitLog());
-                if (isNewCommit(repo, commit)) {
-                    commit.setCollectorItemId(repo.getId());
-                    commitRepository.save(commit);
-                    commitCount++;
-                }
-            }
+		clean(collector);
+		for (GitHubRepo repo : enabledRepos(collector)) {
+			boolean firstRun = false;
+			if (repo.getLastUpdateTime() == null) firstRun = true;
+			repo.setLastUpdateTime(new Date());
+			gitHubRepoRepository.save(repo);
+			LOG.debug(repo.getOptions().toString()+"::"+repo.getBranch());
+			for (Commit commit : gitHubClient.getCommits(repo, firstRun)) {
+				LOG.debug(commit.getTimestamp()+":::"+commit.getScmCommitLog());
+				if (isNewCommit(repo, commit)) {
+					commit.setCollectorItemId(repo.getId());
+					commitRepository.save(commit);
+					commitCount++;
+				}
+			}
 
-            repoCount++;
-        }
-        log("Repo Count", start, repoCount);
-        log("New Commits", start, commitCount);
+			repoCount++;
+		}
+		log("Repo Count", start, repoCount);
+		log("New Commits", start, commitCount);
 
-        log("Finished", start);
-    }
+		log("Finished", start);
+	}
 
-    @SuppressWarnings("unused")
-    private Date lastUpdated(GitHubRepo repo) {
-        return repo.getLastUpdateTime();
-    }
+	@SuppressWarnings("unused")
+	private Date lastUpdated(GitHubRepo repo) {
+		return repo.getLastUpdateTime();
+	}
 
-    private List<GitHubRepo> enabledRepos(Collector collector) {
-        return gitHubRepoRepository.findEnabledGitHubRepos(collector.getId());
-    }
+	private List<GitHubRepo> enabledRepos(Collector collector) {
+		return gitHubRepoRepository.findEnabledGitHubRepos(collector.getId());
+	}
 
-    private boolean isNewCommit(GitHubRepo repo, Commit commit) {
-        return commitRepository.findByCollectorItemIdAndScmRevisionNumber(
-                repo.getId(), commit.getScmRevisionNumber()) == null;
-    }
+	private boolean isNewCommit(GitHubRepo repo, Commit commit) {
+		return commitRepository.findByCollectorItemIdAndScmRevisionNumber(
+				repo.getId(), commit.getScmRevisionNumber()) == null;
+	}
 
 
 }
